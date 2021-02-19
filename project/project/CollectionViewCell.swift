48f41e224f924a93ba03e80531bc9b961baeac72
Esraa-MG <77212235+Esraa-MG@users.noreply.github.com>
2021-02-19 23:31:23 +0000

    first

---
+++ project/project/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  project
+//
+//  Created by iOS Training on 2/19/21.
+//  Copyright © 2021 iOS Training. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
